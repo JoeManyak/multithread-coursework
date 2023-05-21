5a3f333c8efb21459b4ba501265915419b39586c
bohdan <bogdanjmyronchuk@gmail.com>
2023-05-21 23:16:23 +0300

    deadlock

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	"multidfs/tree"
+	"time"
 )
 
-const depth = 3
+const depth = 10
 
 func main() {
+	fmt.Println("Tree Generating...")
 	t := tree.GenerateTree(depth)
-	fmt.Println(t)
+
+	//for {
+	fmt.Println("--------------------")
+	fmt.Println("Single Thread Search...")
+	now := time.Now()
+	fmt.Println(t.SingleThreadSearch())
+	fmt.Println("Single thread estimate: ", time.Since(now))
+
+	fmt.Println("Multi Thread Search...")
+	now = time.Now()
+	fmt.Println(t.MultiThreadSearch(5))
+	fmt.Println("Multi thread estimate: ", time.Since(now))
+	//}
+	for {
+	}
 }
--- tree/tree.go
+++ tree/tree.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"math/rand"
+	"sync"
 )
 
 var r = rand.New(rand.NewSource(0))
@@ -15,7 +16,7 @@ type Node struct {
 func GenerateTree(depth int) Node {
 	MainNode := Node{IsNeeded: false}
 	MainNode.setupChildren(depth)
-	MainNode.generateSearchPlace(depth)
+	MainNode.generateSearchPlace(depth / 2)
 	return MainNode
 }
 
@@ -34,11 +35,88 @@ func (n *Node) setupChildren(depthLeft int) {
 func (n *Node) generateSearchPlace(depthLeft int) {
 	if depthLeft <= 0 {
 		n.IsNeeded = true
+		return
 	}
 	move := r.Intn(2)
 	if move == 1 {
 		n.Left.generateSearchPlace(depthLeft - 1)
 	} else {
 		n.Right.generateSearchPlace(depthLeft - 1)
+	}
+}
+
+func (n *Node) SingleThreadSearch() *Node {
+	res := make(chan *Node, 1)
+	n.singleThreadSearchUtil(res)
+	select {
+	case node := <-res:
+		return node
+	default:
+		return nil
+	}
+}
+
+func (n *Node) singleThreadSearchUtil(res chan *Node) {
+	if n.IsNeeded {
+		res <- n
+		return
+	}
+	if n.Left != nil {
+		n.Left.singleThreadSearchUtil(res)
+	}
+	if n.Right != nil {
+		n.Right.singleThreadSearchUtil(res)
+	}
+}
+
+func (n *Node) MultiThreadSearch(threads int) *Node {
+	wg := sync.WaitGroup{}
+	ch := make(chan *Node, 1)
+	threadController := make(chan struct{}, threads)
+
+	stopper := false
+
+	wg.Add(1)
+
+	threadController <- struct{}{}
+	go n.multiThreadSearchUtil(&wg, ch, &stopper, threadController)
+
+	waiter := make(chan struct{})
+	go waitAsChan(waiter, &wg)
+
+	select {
+	case res := <-ch:
+		return res
+	case _ = <-waiter:
+		return nil
+	}
+}
+
+func waitAsChan(ch chan struct{}, wg *sync.WaitGroup) {
+	wg.Wait()
+	ch <- struct{}{}
+}
+
+func (n *Node) multiThreadSearchUtil(wg *sync.WaitGroup, ch chan *Node, stopper *bool, controller chan struct{}) {
+	defer func() {
+		<-controller
+		wg.Done()
+	}()
+
+	if n.IsNeeded {
+		*stopper = true
+		ch <- n
+		close(controller)
+		return
+	}
+	if n.Left != nil {
+		wg.Add(1)
+		controller <- struct{}{}
+		go n.Left.multiThreadSearchUtil(wg, ch, stopper, controller)
+	}
+	if n.Right != nil {
+		wg.Add(1)
+		controller <- struct{}{}
+		go n.Right.multiThreadSearchUtil(wg, ch, stopper, controller)
 	}
 }
